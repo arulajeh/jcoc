792406e816d4740e0d002a338adc97b6d60218a0
Mutiara Rizkyna <mutiararizkyna54@gmail.com>
2019-08-06 14:52:30 +0700

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-<<<<<<< HEAD
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-
-=======
   { path: 'add-music', loadChildren: './pages/add-music/add-music.module#AddMusicPageModule' },
->>>>>>> 7ea4420b81ce4fa070f64e30174d43de78c438eb
-
+  { path: 'login', loadChildren: './pages/login/login.module#loginPageModule' }
 ];
 
 @NgModule({
