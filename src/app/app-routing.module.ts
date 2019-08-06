6e43c3ec8cd672cfcbbd098a54820ac7990ebcd5
mfajarps <mfajarputras@gmail.com>
2019-08-06 15:25:47 +0700

    waw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'add-music', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'add-music', loadChildren: './pages/add-music/add-music.module#AddMusicPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#loginPageModule' }
