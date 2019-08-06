ec8ce3f9102f02cd1c972aeeaa8a13783d952620
mfajarps <mfajarputras@gmail.com>
2019-08-06 14:08:53 +0700

    make it better

---
+++ src/app/pages/add-music/add-music.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AddMusicPage } from './add-music.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AddMusicPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [AddMusicPage]
+})
+export class AddMusicPageModule {}
