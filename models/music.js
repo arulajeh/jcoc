c9837fc946bf9b3b9660cfc2cd4f984d16ed0d31
Syahrulloh <syahrulloh3@gmail.com>
2019-08-07 23:41:07 +0700

    field penyanyi

--- models/music.js
+++ models/music.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      singer: {
+      penyanyi: {
         type: DataTypes.STRING,
         allowNull: false,
       },
