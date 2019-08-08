1a74dff7935446fb00347365a430b7f021a02bb8
Rafi Pratama Merta <pratamamertar@gmail.com>
2019-08-08 09:06:19 +0700

    allmusic page

--- src/app/pages/allmusic/allmusic.page.ts
+++ src/app/pages/allmusic/allmusic.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-allmusic',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllmusicPage implements OnInit {
 
-  constructor() { }
+  //trustedVideoUrl: SafeResourceUrl;
+  
+  music_lists =[
+    {
+      "name" : "Skip The Use",
+      "title" : "Nameless World",
+      "link" : "https://www.youtube.com/embed/OHXf7wEpBPI"
+    },
+    {
+      "name" : "Skip The Use",
+      "title" : "Nameless World",
+      "link" : "https://www.youtube.com/embed/OHXf7wEpBPI"
+    },
+    {
+      "name" : "Skip The Use",
+      "title" : "Nameless World",
+      "link" : "https://www.youtube.com/embed/OHXf7wEpBPI"
+    },
+    {
+      "name" : "Skip The Use",
+      "title" : "Nameless World",
+      "link" : "https://www.youtube.com/embed/OHXf7wEpBPI"
+    },
+    {
+      "name" : "Skip The Use",
+      "title" : "Nameless World",
+      "link" : "https://www.youtube.com/embed/OHXf7wEpBPI"
+    },
+    {
+      "name" : "Skip The Use",
+      "title" : "Nameless World",
+      "link" : "https://www.youtube.com/embed/OHXf7wEpBPI"
+    },
+    {
+      "name" : "Skip The Use",
+      "title" : "Nameless World",
+      "link" : "https://www.youtube.com/embed/OHXf7wEpBPI"
+    },
+  ]
+
+  constructor(public navCtrl: NavController,
+    private domSanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
+
+  // ionViewWillEnter(): void {
+  //   for(let i of this.music_lists){
+  //     this.trustedVideoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(i.link);
+  //   }
+  // } 
 
 }
