00458f700ad80a92d8293fc895e6628bcdaf3628
muhamadbagussantoso <muhamadbagussantoso@gmail.com>
2019-09-14 15:59:51 +0700

    footer tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'all-music', loadChildren: './pages/allmusic/allmusic.module#AllmusicPageModule' },
   { path: 'music-detail', loadChildren: './pages/music-detail/music-detail.module#MusicDetailPageModule' },
   { path: 'view-lyric', loadChildren: './pages/view-lyric/view-lyric.module#ViewLyricPageModule' },
   { path: 'edit-music', loadChildren: './pages/edit-music/edit-music.module#EditMusicPageModule' },
